c7ea435d67061e5a03f943cfe350505a3b91904c
Tim Wilkins <tim.f.wilkins@gmail.com>
2019-04-22 22:12:14 -0500

    ...

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav')
+const menuNav = document.querySelector('.menu-nav');
+const main = document.querySelector('main');
 //set initial state of menu
 let showMenu = false;
 menuBtn.addEventListener('click',toggleMenu)
@@ -9,6 +10,7 @@ function toggleMenu() {
     menuBtn.classList.add('close');
     menu.classList.add('show');
     menuNav.classList.add('show');
+    main.classList.add('blur');
 
     //set menu state
     showMenu = true;
@@ -16,7 +18,10 @@ function toggleMenu() {
     menuBtn.classList.remove('close');
     menu.classList.remove('show');
     menuNav.classList.remove('show');
+    main.classList.remove('blur');
     //set menu state
     showMenu = false;
   }
 }
+
+// TweenMax.from("body", 1, {color:"white"}, 0.5);
